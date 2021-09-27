c6931a13e486106133a53b6213c3a500efa66434
Todd Bruss <admin@starplayrx.com>
2021-09-27 14:56:10 -0400

    iOS 14 async away..

--- IPTVee/IPTVee/ViewController.swift
+++ IPTVee/IPTVee/ViewController.swift
@@ -28,15 +28,18 @@ class ViewController: UIViewController {
 
     var cats: Categories? = nil
     var conf: Configuration? = nil
-
+    
     func getCategories() {
         let getCats = Actions.getLiveCategoriesAction.rawValue
         let endpoint = api.getEndpoint(creds, iptv, getCats)
-        
+            
         rest.getRequest(endpoint: endpoint) {  (categories) in
-            guard let categories = categories else { return }
+            guard let categories = categories else {
+                self.asyncRan = true
+                return
+            }
             self.cats = try? self.decoder.decode(Categories.self, from: categories)
-            print(self.cats)
+            self.asyncRan = true
         }
     }
     
@@ -45,19 +48,40 @@ class ViewController: UIViewController {
         let endpoint = api.getEndpoint(creds, iptv, getConfig)
         
         rest.getRequest(endpoint: endpoint) { (config) in
-            guard let config = config else { return }
+
+            guard let config = config else {
+                self.asyncRan = true
+                return
+            }
             
-            let decoder = JSONDecoder()
-            self.conf = try? decoder.decode(Configuration.self, from: config)
-            print(self.conf)
+            self.conf = try? self.decoder.decode(Configuration.self, from: config)
+            self.asyncRan = true
         }
     }
     
- 
+    var asyncRan = false
+    
+    func awaitRan(action: String) {
+        DispatchQueue.global().async { [unowned self] in
+            while !self.asyncRan {}
+            if action == Actions.getLiveCategoriesAction.rawValue {
+                print("1")
+                print(self.conf)
+                getCategories()
+                asyncRan = false
+                awaitRan(action: "")
+            } else if action.isEmpty {
+                print("2")
+                print(self.cats)
+            }
+        }
+    }
     
     @IBAction func Login(_ sender: Any) {
+        asyncRan = false
         getConfig()
-        getCategories()
+        awaitRan(action: Actions.getLiveCategoriesAction.rawValue)
+
     }
     
     
